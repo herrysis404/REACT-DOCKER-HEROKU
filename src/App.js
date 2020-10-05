3273689606f57f7114993a9c8161f2f8586f3a05
Arthit Kanjai <arthit.kanjai@nxp.com>
2020-10-05 08:57:03 +0700

    Initial all

--- src/App.js
+++ src/App.js
@@ -2,18 +2,11 @@ import React from 'react';
 import { Calendar } from './Calendar';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { LoginCallback, SecureRoute, Security } from '@okta/okta-react';
 
 function App() {
   return (
     <Router>
-      <Security issuer='https://{YourOktaDomain}/oauth2/default'
-                clientId='{ClientId}'
-                redirectUri={window.location.origin + '/callback'}
-                pkce={true}>
-        <SecureRoute path='/' exact={true} component={Calendar}/>
-        <Route path='/callback' component={LoginCallback}/>
-      </Security>
+        <Route path='/' exact={true} component={Calendar}/>
     </Router>
   );
 }
